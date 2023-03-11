61ea34e1c783f74bd104ebb022022a138e311739
Sapfir0 <sapfir999999@yandex.ru>
2023-03-11 19:57:51 +0300

    Updated cases

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from './components/Button/B'
+export * from './components/Button/Button'
 export * from './components/Card/Card'
 export * from './components/Icon/Icon'
 export * from './components/List/List'
